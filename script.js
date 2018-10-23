05f24edec23c1ac03ac2b1198fde4a4c27827846
Michael Smith <smithms91@gmail.com>
2018-10-23 11:08:27 -0400

    Added a Section & Scroll To Top

--- script.js
+++ script.js
@@ -22,5 +22,18 @@ $(document).ready(function() {
     $(".hidden.blog").slideUp();
     $(".hidden.features").slideToggle();
   });
+
+  $(window).scroll(function(){
+    if ($(this).scrollTop() > 50) {
+        $('#backToTop').fadeIn('slow');
+    } else {
+        $('#backToTop').fadeOut('slow');
+    }
+  });
+  
+  $('#backToTop').click(function(){
+      $("html, body").animate({ scrollTop: 0 }, 500);
+      return false;
+  });
   
 });
